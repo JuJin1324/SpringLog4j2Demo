d0796d8ba82893429773873e4c20a44505be8a8f
jujin <jujin1324@daum.net>
2019-05-02 15:36:18 +0900

    log check를 위한 ApplicationRunner 인터페이스 구현

--- src/main/java/spring/boot/log4j2/demo/DemoApplication.java
+++ src/main/java/spring/boot/log4j2/demo/DemoApplication.java
@@ -1,18 +1,24 @@
 package spring.boot.log4j2.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
-public class DemoApplication {
+public class DemoApplication implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.debug("[jujiny] Log Check for DEBUG");
+        log.info("[jujiny] Log Check for INFO");
+        log.error("[jujiny] Log Check for ERROR");
+    }
 
     public static void main(String[] args) {
-
-        log.debug("[jujiny] Starting Spring Boot Application...");
         SpringApplication.run(DemoApplication.class, args);
-
     }
 
 }
